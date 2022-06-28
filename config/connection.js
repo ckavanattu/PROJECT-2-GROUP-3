1c0a8a402ead9ca2f6a539599462dfc1e77cfb89
ckavanattu <chackojk@gmail.com>
2022-06-27 20:19:37 -0700

    created user model and routes for new users

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+
+// create connection to database
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+module.exports = sequelize;
