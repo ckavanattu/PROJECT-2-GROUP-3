a79f77a1f64c070e0589676fc2acef88503fb62b
hvphan <hvphan1993@gmail.com>
2022-06-29 11:33:33 -0700

    added spikeballs every round, need to adjust player hitbox, possibly add game over/restart screen

--- gameFolder/src/index.js
+++ gameFolder/src/index.js
@@ -19,7 +19,10 @@ class MyGame extends Phaser.Scene
         this.load.image("mountain", mountain);
         this.load.image("ground", ground);
         this.load.image("diamond", diamond);
-        this.load.image("spikeball", spikeball);
+        this.load.image("spikeball", spikeball, {
+            frameWidth: 32,
+            frameHeight: 32,
+        });
         
         this.load.spritesheet("movement", move, {
             frameWidth: 32, 
@@ -51,7 +54,10 @@ class MyGame extends Phaser.Scene
         platforms.create(550, 220, "ground")
         platforms.create(-120, 150, "ground")
         platforms.create(600, 450, "ground")
+
         this.player = this.physics.add.sprite(100, 450, "movement");
+        this.player.setSize(20, 40, true);
+        
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
 
@@ -75,18 +81,60 @@ class MyGame extends Phaser.Scene
             repeat: -1,
         });
 
+        // diamonds
         const diamonds = this.physics.add.group({
             key: "diamond",
             repeat: 11,
-            setXY: {x: 12, y: 0, stepX: 70 },
+            setXY: {x: 12, y: 0, stepX: 68 },
         });
         diamonds.children.iterate(function(child) {
             child.setBounceY(Phaser.Math.FloatBetween(0.4,0.8));
         });
         this.physics.add.collider(diamonds, platforms);
         this.physics.add.overlap(this.player, diamonds, collect, null, this);
+
+        // spikeballs
+        const spikeballs = this.physics.add.group();
+        
+        this.physics.add.collider(spikeballs, platforms);
+
+        this.physics.add.collider(this.player, spikeballs, spikeballTouched, null, this);
+        
+        function spikeballTouched(player, spikeball) {
+            this.physics.pause();
+            this.player.setTint(0xff000);
+            this.player.anims.play("turn");
+        }
+
+        // score text
+        const scoreText = this.add.text(16, 16, "score: 0", {
+            fontSize: "32px",
+            fill: "#000"
+        });
+        let score = 0;
+
+        // diamonds collision
         function collect(player, diamond) {
             diamond.disableBody(true, true);
+            score+=10;
+            scoreText.setText("Score: " + score);
+
+            if (diamonds.countActive(true) === 0) {
+                diamonds.children.iterate(function(child) {
+                    child.enableBody(true, child.x, 0, true, true);
+                });
+
+                var x = 
+                    player.x <400 
+                        ? Phaser.Math.Between(400,800) 
+                        : Phaser.Math.Between(0, 400);
+                
+                const spikeball = spikeballs.create(x, 16, "spikeball");
+                spikeball.setBounce(1);
+                spikeball.setCollideWorldBounds(true)
+                spikeball.setVelocity(Phaser.Math.Between(-200, 200), 20)
+
+            }
         }
     }
 
@@ -119,7 +167,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: {y: 450},
-            debug: false
+            debug: true
         }
     },
     scene: MyGame
