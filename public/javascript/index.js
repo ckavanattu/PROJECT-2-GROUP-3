69572756d5d8e623012c02428c8aadd8ad03576a
hvphan <hvphan1993@gmail.com>
2022-07-05 22:21:41 -0700

    corrected pathing, cleaned up code a bit, prepping for level2 and level3 editing

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,33 +1,11 @@
-// import Phaser from "phaser";
-// import mountain from "./assets/mountain1.jpg";
-// import ground from "./assets/platform.png";
-// import diamond from "./assets/diamond.png";
-// import spikeball from "./assets/spikeball.png";
-// import move from "./assets/movement.png";
-// import night_scene from "./assets/night_scene.gif";
-// import pixel_castle from "./assets/pixel_castle.jpg";
 
-const config = {
-    type: Phaser.AUTO,
-    parent: "phaser-example",
-    width: 800,
-    height: 600,
-    physics: {
-      default: "arcade",
-      arcade: {
-        gravity: { y: 450 },
-        debug: true,
-      },
-    },
-    scene: [level1, level2, level3 ]
-  };
-  
+
 
 class level1 extends Phaser.Scene {
   constructor() {
-    super({key: 'level1'});
+    super({key: "level1"});
   }
-
+  
   preload() {
     this.load.image("mountain", "assets/mountain1.jpg");
     // this.load.image("night_scene", "assets/night_scene.gif");
@@ -38,36 +16,36 @@ class level1 extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32,
     });
-
+  
     this.load.spritesheet("movement", "assets/movement.png", {
       frameWidth: 48,
       frameHeight: 64,
     });
-
+  
   }
-
+  
   create() {
     this.add.image( 400, 300, "mountain");
     
-
+  
     const platforms = this.physics.add.staticGroup();
     // platforms
     platforms.create(400, 568, "ground").setScale(2).refreshBody();
-
+  
     platforms.create(900, 350, "ground");
     platforms.create(25, 295, "ground");
     platforms.create(550, 220, "ground");
     platforms.create(-120, 150, "ground");
     platforms.create(600, 450, "ground");
-
+  
     this.player = this.physics.add.sprite(100, 450, "movement");
     this.player.setSize(26, 42, true);
     this.player.body.offset.y = 22;
     this.player.body.offset.x = 18;
-
+  
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
-
+  
     this.physics.add.collider(this.player, platforms);
     // animation
     this.anims.create({
@@ -87,7 +65,7 @@ class level1 extends Phaser.Scene {
       frameRate: 5,
       repeat: -1,
     });
-
+  
     // diamonds
     const diamonds = this.physics.add.group({
       key: "diamond",
@@ -99,12 +77,12 @@ class level1 extends Phaser.Scene {
     });
     this.physics.add.collider(diamonds, platforms);
     this.physics.add.overlap(this.player, diamonds, collect, null, this);
-
+  
     // spikeballs
     const spikeballs = this.physics.add.group();
-
+  
     this.physics.add.collider(spikeballs, platforms);
-
+  
     this.physics.add.collider(
       this.player,
       spikeballs,
@@ -112,7 +90,7 @@ class level1 extends Phaser.Scene {
       null,
       this
     );
-
+  
     // lose when hit by spikeball
     function spikeballTouched(player, spikeball) {
       this.physics.pause();
@@ -123,7 +101,7 @@ class level1 extends Phaser.Scene {
       this.restartText.visible = true;
       this.input.on('pointerdown', () => this.scene.start('level1'));
     }
-
+  
     // score text
     const scoreText = this.add.text(16, 16, "score: 0", {
       fontSize: "32px",
@@ -131,7 +109,7 @@ class level1 extends Phaser.Scene {
       
     });
     let score = 0;
-
+  
     // if (score > 100) {
     //   this.cameras.main.fadeOut(1000, 0, 0, 0);
     //   this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
@@ -139,54 +117,55 @@ class level1 extends Phaser.Scene {
     //     this.scene.start('level2', level2);
     //   })
     // }
-
+  
     // diamonds collision
     function collect(player, diamond) {
       diamond.disableBody(true, true);
       score += 10;
       scoreText.setText("Score: " + score);
-
+  
       if (score > 10) {
-        this.cameras.main.fadeOut(1000, 0, 0, 0);
-        this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
-        //   this.scene.add('level2');
-          this.scene.start('level2');
-          // this.background = this.add.image(400, 300, "pixel_castle").setOrigin(0.5, 0.5);
-          // this.background.displayWidth = 800;
-          // this.background.displayHeight = 600;
-          // this.cameras.main.fadeIn(1000, 0, 0, 0)
+        this.scene.start('level2');
+        // this.cameras.main.fadeOut(1000, 0, 0, 0);
+        // this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+        // //   this.scene.add('level2');
           
-        })
+        //   // this.background = this.add.image(400, 300, "pixel_castle").setOrigin(0.5, 0.5);
+        //   // this.background.displayWidth = 800;
+        //   // this.background.displayHeight = 600;
+        //   // this.cameras.main.fadeIn(1000, 0, 0, 0)
+          
+        // })
       }
-
+  
       if (diamonds.countActive(true) === 0) {
         diamonds.children.iterate(function (child) {
           child.enableBody(true, child.x, 0, true, true);
         });
-
+  
         var x =
           player.x < 400
             ? Phaser.Math.Between(400, 800)
             : Phaser.Math.Between(0, 400);
-
+  
         const spikeball = spikeballs.create(x, 16, "spikeball");
         spikeball.setBounce(1);
         spikeball.setCollideWorldBounds(true);
         spikeball.setVelocity(Phaser.Math.Between(-200, 200), 20);
       }
     }
-
+  
     // game over text
     this.gameOverText = this.add.text(400,280, 'Game Over', { fontSize: '86px', fill: '#000'})
     this.gameOverText.setOrigin(0.5);
     this.gameOverText.visible = false;
-
+  
     this.restartText = this.add.text(400,375, 'Click Screen to Restart', { fontSize: '44px', fill: '#000'})
     this.restartText.setOrigin(0.5);
     this.restartText.visible = false;
-
+  
   }
-
+  
   update() {
     const cursors = this.input.keyboard.createCursorKeys();
     if (cursors.left.isDown) {
@@ -201,30 +180,30 @@ class level1 extends Phaser.Scene {
       this.player.setVelocityX(0);
       this.player.anims.play("turn");
     }
-
+  
     if (cursors.up.isDown && this.player.body.touching.down) {
       this.player.setVelocityY(-400);
     }
-
+  
   }
-}
-
-// const config = {
-//   type: Phaser.AUTO,
-//   parent: "phaser-example",
-//   width: 800,
-//   height: 600,
-//   physics: {
-//     default: "arcade",
-//     arcade: {
-//       gravity: { y: 450 },
-//       debug: true,
-//     },
-//   },
-//   scene: [level1, level2 ]
-// };
-
-class level2 extends Phaser.Scene {
+  }
+  
+  // const config = {
+  //   type: Phaser.AUTO,
+  //   parent: "phaser-example",
+  //   width: 800,
+  //   height: 600,
+  //   physics: {
+  //     default: "arcade",
+  //     arcade: {
+  //       gravity: { y: 450 },
+  //       debug: true,
+  //     },
+  //   },
+  //   scene: [level1, level2 ]
+  // };
+  
+  class level2 extends Phaser.Scene {
     constructor() {
       super({key: "level2"});
     }
@@ -237,7 +216,7 @@ class level2 extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32,
     });
-
+  
     this.load.spritesheet("movement", "assets/movement.png", {
       frameWidth: 48,
       frameHeight: 64,
@@ -246,9 +225,10 @@ class level2 extends Phaser.Scene {
     }
   
     create() {
-
+  
     this.cameras.main.fadeIn(1000, 0, 0, 0)
     
+    // this.add.image( 400, 300, "pixel_castle");
     this.background = this.add.image(400, 300, "pixel_castle").setOrigin(0.5, 0.5);
     this.background.displayWidth = 800;
     this.background.displayHeight = 600;
@@ -342,9 +322,9 @@ class level2 extends Phaser.Scene {
         
         if (score > 10) {
             this.cameras.main.fadeOut(1000, 0, 0, 0);
-            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
-              this.scene.start('level3');             
-            })
+            this.scene.start('level3');             
+            // this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+            // })
           }
   
         if (diamonds.countActive(true) === 0) {
@@ -395,8 +375,8 @@ class level2 extends Phaser.Scene {
       }
     }
   };
-
-class level3 extends Phaser.Scene {
+  
+  class level3 extends Phaser.Scene {
     constructor() {
       super({key: 'level3'});
     }
@@ -420,7 +400,7 @@ class level3 extends Phaser.Scene {
     }
   
     create() {
-      this.add.image( 400, 300, "mountain");
+      this.add.image( 400, 300, "night_scene");
       
   
       const platforms = this.physics.add.staticGroup();
@@ -583,5 +563,19 @@ class level3 extends Phaser.Scene {
     }
   }
   
-
-const game = new Phaser.Game(config);
+  const config = {
+    type: Phaser.AUTO,
+    parent: "phaser-example",
+    width: 800,
+    height: 600,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 450 },
+        debug: true,
+      },
+    },
+    scene: [level1, level2, level3 ]
+  };
+  
+  const game = new Phaser.Game(config);
